b457ef8d2f3670171f0b6ff57d73fffb0ea17695
gaodehan <573308611@qq.com>
2018-09-23 14:22:14 +0800

    v-for 用法以及index技术盲点 待解决

--- projectName/src/App.vue
+++ projectName/src/App.vue
@@ -1,13 +1,36 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <!-- <p :title='hello'>
+      {{ hello }}
+    </p> -->
+  <p v-text="hello"></p>
+  <p v-html="hello"></p>
+  <!--使用v-for 循环 技术盲点是怎么获取到每个集合元素的索引-->
+  <p v-for="item in list" :key="item.id"> {{ item.name }} - {{ item.price }} - {{ item.id }}</p> 
+
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  data() {
+      return {
+        hello: 'world',
+        list: [
+          { id:'1',
+            name: 'apple',
+            price: 34
+          },
+          { id: '2',
+            name: 'banana',
+            price: 56
+          }
+        ]
+      }
+ }
+
 }
 </script>
 
