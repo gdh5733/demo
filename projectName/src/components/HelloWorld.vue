471cdaba379801e039b93e5f3560ce1e86b9ee19
gaodehan <573308611@qq.com>
2020-02-22 14:48:24 +0800

    提交代码

--- projectName/src/components/HelloWorld.vue
+++ projectName/src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  
+<div id="">
 <el-form ref="form" :model="form" label-width="80px">
   <el-form-item label="活动名称">
     <el-input v-model="form.name"></el-input>
@@ -44,7 +44,7 @@
     <el-button>取消</el-button>
   </el-form-item>
 </el-form>
-
+</div>
 </template>
 
 <script>
